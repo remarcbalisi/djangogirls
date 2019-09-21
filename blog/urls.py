b3a7e12e9033f26d29c35b504a76a0dec0e7ae45
Remarc Balisi <remarc.balisi@fligno.com>
2019-09-21 13:07:31 +0800

    editing of blog: ok

--- blog/urls.py
+++ blog/urls.py
@@ -6,4 +6,6 @@ from django.conf.urls import url
 urlpatterns = [
     path('', views.post_list, name='post_list'),
     url(r'^(?P<pk>[0-9]+)/$', views.post_detail, name='post_detail'),
+    url(r'^edit/(?P<pk>[0-9]+)/$', views.post_edit, name='post_edit'),
+    path('post/new', views.post_new, name='post_new'),
 ]
