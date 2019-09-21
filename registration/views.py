18bbe82ea79feeb8eaa46fd6f50cfafedf49b6da
Remarc Balisi <remarc.balisi@fligno.com>
2019-09-21 16:03:39 +0800

    add group in registering user

--- registration/views.py
+++ registration/views.py
@@ -2,7 +2,7 @@ from django.contrib.auth import authenticate, login
 from django.shortcuts import render, redirect
 
 from registration.forms import SignUpForm
-
+from django.contrib.auth.models import Group
 
 def register(request):
     if request.method == 'POST':
@@ -12,6 +12,9 @@ def register(request):
             username = form.cleaned_data.get('username')
             raw_password = form.cleaned_data.get('password1')
             user = authenticate(username=username, password=raw_password)
+
+            guest_group = Group.objects.get(name='Guest')
+            guest_group.user_set.add(user)
             login(request, user)
             return redirect('post_list')
     else:
