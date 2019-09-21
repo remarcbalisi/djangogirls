6b102606a4e0f3eb65b2f54da22e4898219f7c57
Remarc Balisi <remarc.balisi@fligno.com>
2019-09-21 12:48:14 +0800

    viewing and listing of blog posts

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from . import views
 
+from django.conf.urls import url
+
 urlpatterns = [
     path('', views.post_list, name='post_list'),
+    url(r'^(?P<pk>[0-9]+)/$', views.post_detail, name='post_detail'),
 ]
